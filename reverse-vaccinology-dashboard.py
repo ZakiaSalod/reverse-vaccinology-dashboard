d5dfed5188813bba15178cdf59577379de55bc5c
Zakia Salod (She/Her) <zakia.salod@gmail.com>
2024-10-05 19:58:49 +0200

    Remove deprecated Streamlit option

--- reverse-vaccinology-dashboard.py
+++ reverse-vaccinology-dashboard.py
@@ -6,7 +6,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import io
-st.set_option('deprecation.showfileUploaderEncoding', False)
 from PIL import Image
 import pandas as pd
 import numpy as np
